85fec47cf89a8b1b323ba5cfd520e2a5eaf8f38e
Tonia Roddick <troddick@USGS.gov>
2016-02-12 10:08:49 -0600

    url redirect to attempted page after login working

--- src/component/event/eventCtrl.js
+++ src/component/event/eventCtrl.js
@@ -3,8 +3,8 @@
 
     var SettingsControllers = angular.module('SettingsControllers');
 
-    SettingsControllers.controller('eventCtrl', ['$scope', '$rootScope', '$cookies', '$location', '$http', '$filter', 'MEMBER', 'allEvents', 'allEventTypes', 'allEventStats',
-        function ($scope, $rootScope, $cookies, $location, $http, $filter, MEMBER, allEvents, allEventTypes, allEventStats) {
+    SettingsControllers.controller('eventCtrl', ['$scope', '$rootScope', '$cookies', '$location', '$http', '$filter', 'MEMBER', 'allCoordMembers', 'allEvents', 'allEventTypes', 'allEventStats',
+        function ($scope, $rootScope, $cookies, $location, $http, $filter, MEMBER, allCoordMembers, allEvents, allEventTypes, allEventStats) {
             if ($cookies.get('STNCreds') === undefined || $cookies.get('STNCreds') === "") {
                 $scope.auth = false;
                 $location.path('/login');
@@ -33,25 +33,22 @@
 
                 $scope.eventTypeList = allEventTypes;
                 $scope.eventStatList = allEventStats;
-                $http.defaults.headers.common.Authorization = 'Basic ' + $cookies.get('STNCreds');
-                $http.defaults.headers.common.Accept = 'application/json';
-                MEMBER.getRoleMembers({ roleId: 1 }).$promise.then(function (response) {
-                    $scope.eventCoordList = response;
-                    $scope.eventList = [];
-                    for (var x = 0; x < allEvents.length; x++) {
-                        var E = {};
-                        E.EVENT_ID = allEvents[x].EVENT_ID;
-                        E.Name = allEvents[x].EVENT_NAME;
-                        E.Type = $scope.eventTypeList.filter(function (a) { return a.EVENT_TYPE_ID == allEvents[x].EVENT_TYPE_ID; })[0].TYPE;
-                        E.Status = $scope.eventStatList.filter(function (r) { return r.EVENT_STATUS_ID == allEvents[x].EVENT_STATUS_ID; })[0].STATUS;
-                        var coord = $scope.eventCoordList.filter(function (c) { return c.MEMBER_ID == allEvents[x].EVENT_COORDINATOR; })[0];
-                        E.StartDate = allEvents[x].EVENT_START_DATE;
-                        E.EndDate = allEvents[x].EVENT_END_DATE;
-                        E.Coord = coord !== undefined ? coord.FNAME + " " + coord.LNAME : "";
+                $scope.eventCoordList = allCoordMembers;
 
-                        $scope.eventList.push(E);
-                    }
-                });
+               $scope.eventList = [];
+               for (var x = 0; x < allEvents.length; x++) {
+                   var E = {};
+                   E.EVENT_ID = allEvents[x].EVENT_ID;
+                   E.Name = allEvents[x].EVENT_NAME;
+                   E.Type = $scope.eventTypeList.filter(function (a) { return a.EVENT_TYPE_ID == allEvents[x].EVENT_TYPE_ID; })[0].TYPE;
+                   E.Status = $scope.eventStatList.filter(function (r) { return r.EVENT_STATUS_ID == allEvents[x].EVENT_STATUS_ID; })[0].STATUS;
+                   var coord = $scope.eventCoordList.filter(function (c) { return c.MEMBER_ID == allEvents[x].EVENT_COORDINATOR; })[0];
+                   E.StartDate = allEvents[x].EVENT_START_DATE;
+                   E.EndDate = allEvents[x].EVENT_END_DATE;
+                   E.Coord = coord !== undefined ? coord.FNAME + " " + coord.LNAME : "";
+
+                   $scope.eventList.push(E);
+               }
             }
         }]);
 }());
